f64c397462e483ac0c1eb1872a1af6bccb8834e4
Tema26 <moskvitin.artem@gmail.com>
2021-03-24 11:28:32 +0300

    #4

--- src/script.js
+++ src/script.js
@@ -1,1 +1,15 @@
-console.log('Hello World')
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("dropdown").classList.toggle("show");
+  }
+  
+  // Закрыть the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    let dropdown = document.getElementById("dropdown");
+      if (dropdown.classList.contains('show')) {
+        dropdown.classList.remove('show');
+      }
+    }
+  }

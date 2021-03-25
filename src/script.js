3ff38880eb939f5d437c2b56ee26d6d83d56eadc
Tema26 <moskvitin.artem@gmail.com>
2021-03-25 15:53:41 +0300

    compare js

--- src/script.js
+++ src/script.js
@@ -1,25 +1,57 @@
 'use strict'
-let el = document.getElementsByClassName('header-menu-nav__item__submenu');
-for(let i=0; i<el.length; i++) {
-    el[i].addEventListener("mouseenter", showSub, false);
-    el[i].addEventListener("mouseleave", hideSub, false);
- }
+// let el = document.getElementsByClassName('header-menu-nav__item__submenu');
+// for(let i=0; i<el.length; i++) {
+//     el[i].addEventListener("mouseenter", showSub, false);
+//     el[i].addEventListener("mouseleave", hideSub, false);
+//  }
 
- function showSub(e) {
-    if(this.children.length>1) {
+//  function showSub(e) {
+//     if(this.children.length>1) {
+//        this.children[1].style.height = "auto";
+//        this.children[1].style.overflow = "visible";
+//        this.children[1].style.opacity = "1";
+//     } else {
+//        return false;
+//     }
+//  }
+//  function hideSub(e) {
+//     if(this.children.length>1) {
+//       this.children[1].style.height = "0px";
+//        this.children[1].style.overflow = "hidden";
+//        this.children[1].style.opacity = "0";
+//     } else {
+//        return false;
+//     }
+// }
+
+function menu(menuItemSelector) {
+   const menuItem = document.querySelectorAll(menuItemSelector);
+   menuItem.forEach(function(item) {
+       item.addEventListener('click' , showSub);
+       item.addEventListener('mouseleave' , hideSub);
+   });
+}
+function showSub() {
+   if(this.children.length > 1) {
+       this.children[1].style.display = 'flex';
+       this.children[1].style.position = 'relative';
        this.children[1].style.height = "auto";
        this.children[1].style.overflow = "visible";
        this.children[1].style.opacity = "1";
-    } else {
+   } else {
        return false;
-    }
- }
- function hideSub(e) {
-    if(this.children.length>1) {
-      this.children[1].style.height = "0px";
+   }
+}
+
+function hideSub() {
+   if(this.children.length > 1) {
+       this.children[1].style.display = 'none';
+       this.children[1].style.height = "0px";
        this.children[1].style.overflow = "hidden";
        this.children[1].style.opacity = "0";
-    } else {
+   } else {
        return false;
-    }
+   }
 }
+
+menu('.header-menu-nav__item');

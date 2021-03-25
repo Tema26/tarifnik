66fae4852a836d202fc56ecfeff85607206dfd2e
Admin <moskvitin.artem@gmail.com>
2021-03-25 07:52:00 +0300

    add some style input box

--- src/script.js
+++ src/script.js
@@ -1,22 +1,25 @@
-function menu(menuItemSelector) {
-  const menuItem = document.querySelectorAll(menuItemSelector);
-  menuItem.forEach(function(item) {
-      item.addEventListener('mouseenter' , showSub);
-      item.addEventListener('mouseleave' , hideSub);
-  });
-}
-function showSub() {
-  if(this.children.length > 1) {
-      this.children[1].style.display = 'flex';
-  } else {
-      return false;
-  }
-}
+'use strict'
+let el = document.getElementsByClassName('header-menu-nav__item__submenu');
+for(let i=0; i<el.length; i++) {
+    el[i].addEventListener("mouseenter", showSub, false);
+    el[i].addEventListener("mouseleave", hideSub, false);
+ }
 
-function hideSub() {
-  if(this.children.length > 1) {
-      this.children[1].style.display = 'none';
-  } else {
-      return false;
-  }
+ function showSub(e) {
+    if(this.children.length>1) {
+       this.children[1].style.height = "auto";
+       this.children[1].style.overflow = "visible";
+       this.children[1].style.opacity = "1";
+    } else {
+       return false;
+    }
+ }
+ function hideSub(e) {
+    if(this.children.length>1) {
+      this.children[1].style.height = "0px";
+       this.children[1].style.overflow = "hidden";
+       this.children[1].style.opacity = "0";
+    } else {
+       return false;
+    }
 }

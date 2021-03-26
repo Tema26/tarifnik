7ec81b2f436301ee9db9c3e76e7922c0580b6635
Tema26 <moskvitin.artem@gmail.com>
2021-03-26 11:43:01 +0300

    add drop menu

--- src/script.js
+++ src/script.js
@@ -33,11 +33,7 @@ function menu(menuItemSelector) {
 }
 function showSub() {
    if(this.children.length > 1) {
-       this.children[1].style.display = 'flex';
-       this.children[1].style.position = 'relative';
-       this.children[1].style.height = "auto";
-       this.children[1].style.overflow = "visible";
-       this.children[1].style.opacity = "1";
+       this.children[1].style.display = 'block';
    } else {
        return false;
    }
@@ -46,9 +42,6 @@ function showSub() {
 function hideSub() {
    if(this.children.length > 1) {
        this.children[1].style.display = 'none';
-       this.children[1].style.height = "0px";
-       this.children[1].style.overflow = "hidden";
-       this.children[1].style.opacity = "0";
    } else {
        return false;
    }
